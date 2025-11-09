3727b336a8b45988fabd2c7598f87246fc4f8483
Anna <a.rcottrill521@gmail.com>
2025-11-09 11:45:21 -0500

    Add wingding translator

---
+++ WingDing Translator/Readme.md
@@ -0,0 +1,22 @@
+# WingDing Translator
+
+In 2015, Undertale was released. It was a really wonderful game and had hundreds 
+of fans. It's now a social pheneonmon with even a Pope having recieved a copy
+as a gift.
+
+However there was one oddity in the game that people took too. In a certain room, 
+there's a deformed skeleton and WingDings pop up on screen. That's the character's
+quirk, each skeleton talks in a goofy font. 
+
+Unlike Papyrus or even Comic Sans, WingDings is very hard to read. Especially these 
+days where it's Unicode characters and not just a matter of changing the font back
+to something easy to read. 
+
+While I could just use a normal translator like those on LingoJam, I have to keep a
+tab open and it's a little too distracting. 
+
+In this folder is a chart is demonstrate how to translate these symbols by hand.
+
+To run this script, install it with TamperMonkey (or any other script modifier).
+
+NOTE: Eventually I will replace that hard to read chart with a better one. 
---
+++ WingDing Translator/Translate.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         WingDings translator
+// @namespace    http://tampermonkey.net/
+// @version      2025-11-09
+// @description  Translates WingDings to English
+// @author       a2937
+// @match        https://**.tumblr.com/**
+// @match        https://discord.com/app/**
+// @match        https://**.archiveofourown.org/**
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @sandbox      DOM
+// ==/UserScript==
+(function() {
+    'use strict';
+     const wingDingAlphabet = {
+        '\u270C': 'A',
+         '\u1F44C': 'B',
+         '\u1F44D': 'C',
+         '\u1F44E': 'D',
+         '\u261C': 'E',
+         '\u261E': 'F',
+         '\u261D': 'G',
+         '\u261F': 'H',
+         '\u270B': 'I',
+         '\u263A': 'J',
+         '\u1F610': 'K',
+         '\u2639': 'L',
+         '\u1F4A3': 'M',
+         '\u2620' : 'N',
+         '\u2690': 'O',
+         '\u1F3F1': 'P',
+         '\u2708': 'Q',
+         '\u263C': 'R',
+         '\u1F4A7': 'S',
+         '\u2744': 'T',
+         '\u1F546': 'U',
+         '\u271E': 'V',
+         '\u1F548': 'W',
+         '\u2720': 'X',
+         '\u2721': 'Y',
+        '\u262A': 'Z',
+         '\u1F4EC': '.',
+        '\u270D': '?',
+         '\u1F4EA': '.',
+
+
+         // Symbol only because for some reason they're not always matching
+         '👍︎': 'C',
+         '😐︎': 'K',
+         '🕆︎': 'U',
+         '💧︎': 'S',
+         '🕈︎': 'W',
+         '📬︎': ".",
+         '📪︎': ','
+     }
+
+
+     document.querySelectorAll('*').forEach((el) => {
+         Object.entries(wingDingAlphabet).forEach(([oldValue, newValue]) => {
+         if (el.innerText && el.innerText.indexOf(`${oldValue}`) >= 0 && el.children.length == 0) {
+                 el.innerText = el.innerText.replaceAll(`${oldValue}`,`${newValue}`);
+             }
+    })
+  });
+    // Your code here...
+})();
