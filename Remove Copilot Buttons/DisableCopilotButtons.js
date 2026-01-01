6fd3ada876e440acb1ec89facb8fd891f6211aff
Anna <a.rcottrill521@gmail.com>
2026-01-01 13:16:13 -0500

    feat: add disable copilot button and Blender script

---
+++ Remove Copilot Buttons/DisableCopilotButtons.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Remove Copilot Buttons
+// @namespace    http://tampermonkey.net/
+// @version      2026-01-01
+// @description  Deletes all Copilot buttons on the Github website
+// @author       nerdychara
+// @match        https://**.github.com/**
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const buttons = document.querySelectorAll(`a[href^="https://github.com/copilot"]`);
+    for(let i = 0 ;i < buttons.length;i++)
+    {
+        buttons[i].remove();
+    }
+    // Your code here...
+})();
