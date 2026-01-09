70500e6acd5b0723a6e8dcacd3d96db3ea25290f
Anna <a.rcottrill521@gmail.com>
2026-01-08 20:42:23 -0500

    feat: remove Ask Reddit button

---
+++ Remove Ask Reddit Button/DisableRedditAiButtons.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Remove Ask Reddit Buttons
+// @namespace    http://tampermonkey.net/
+// @version      2026-01-01
+// @description  Deletes all Ask Reddit buttons on the Reddit website
+// @author       nerdychara
+// @match        https://**.reddit.com/**
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const buttons = document.querySelectorAll(`a[href^="/answers"]`);
+    for(let i = 0 ;i < buttons.length;i++)
+    {
+        buttons[i].remove();
+    }
+})();
