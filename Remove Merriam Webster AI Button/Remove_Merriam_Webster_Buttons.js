144ba9c4a4d5b5ef045f0609ca31f7dfebcbf3e9
Anna <a.rcottrill521@gmail.com>
2026-01-10 13:20:29 -0500

    feat: remove dictionary chatbot

--- Remove Merriam Webster AI Button/Remove_Merriam_Webster_Buttons.js
+++ Remove Merriam Webster AI Button/Remove_Merriam_Webster_Buttons.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Remove Copilot Buttons
+// @name         Remove Merriam Webster Buttons
 // @namespace    http://tampermonkey.net/
 // @version      2026-01-01
-// @description  Deletes all Copilot buttons on the Github website
+// @description  Deletes the Chatbot button on Merriam Webster
 // @author       nerdychara
-// @match        https://**.github.com/**
+// @match        https://**.merriam-webster.com/**
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
@@ -22,10 +22,10 @@ else
 
 function main() {
     'use strict';
-    const buttons = document.querySelectorAll(`a[href^="/copilot"]`);
+    const buttons = document.querySelectorAll(`a[href^="/chatbot"]`);
     for(let i = 0 ;i < buttons.length;i++)
     {
         buttons[i].remove();
     }
-    // Your code here...
 };
+
